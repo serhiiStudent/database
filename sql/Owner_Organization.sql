2dd369454eaa3396aeb3f7193a11a64687bda412
Seroga <sergiycheck@gmail.com>
2019-10-09 22:00:48 +0300

    add connection and pdf diagram

--- sql/Owner_Organization.sql
+++ sql/Owner_Organization.sql
@@ -8,3 +8,7 @@ create table Owner_Organization
 alter table Owner_Organization
   add constraint organization_id_fk foreign key (organization_id)
   references Organization(organization_id )
+
+alter table Owner_Organization
+  add constraint owner_id_fk foreign key (owner_id )
+  references Owner(owner_id  );
